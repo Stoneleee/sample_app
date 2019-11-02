60f324bb50ed4b4aca88badf00f0ae59c9816ee6
stoneleee <weflylee@163.com>
2019-11-02 19:04:45 +0800

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+      base_title = "示例应用"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " | " + base_title
+      end
+  end
 end
