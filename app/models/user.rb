5b6ccceb0ebd4fd4b813999bdb52c70775a8a1a7
stoneleee <weflylee@163.com>
2019-11-05 13:58:59 +0800

    delete attr in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  attr_accessor :password, :password_confirmation
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
